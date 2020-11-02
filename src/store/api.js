5a940a78af28bc07f0d5b157a5c16f0eac92ccc4
an4morph <avgustin456@gmail.com>
2020-11-02 21:55:24 +0600

    add env for proxy

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'query-string'
 
 const OPW_ENDPOINT = 'https://api.openweathermap.org/data/2.5'
-const GP_ENDPOINT = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place'
+const GP_ENDPOINT = `${process.env.REACT_APP_CORS_PROXY}https://maps.googleapis.com/maps/api/place`
 const OPW_API_KEY = process.env.REACT_APP_OPW_API_KEY
 const GP_API_KEY = process.env.REACT_APP_GP_API_KEY
 
