b86b465e2a2705e143dd7d12b506def2d5ee82f0
an4morph <avgustin456@gmail.com>
2020-11-02 22:53:19 +0600

    remove focus styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,12 +12,6 @@ const GlobalStyle = createGlobalStyle`
     -webkit-tap-highlight-color: transparent;
   }
 
-  input, textarea, button {
-    &:focus {
-      outline: none;
-      box-shadow: 0px 0px 0px 2px ${({ theme }) => theme.colors.link};
-    }
-  }
   a {
     color: inherit;
     text-decoration: none;
